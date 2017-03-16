560e26ba1cd311ad1229b5db98328d294bd104ee
andela-akhenda <joseph.akhenda@andela.com>
2017-03-16 20:28:38 +0300

    feat(requests) add records to request_skills

--- app/Http/Controllers/RequestsController.php
+++ app/Http/Controllers/RequestsController.php
@@ -6,17 +6,26 @@ use Illuminate\Http\Response;
 class RequestsController extends Controller {
 
     const MODEL = "App\Requests";
+    const MODEL2 = "App\RequestSkills";
 
     use RESTActions;
 
     public function add(Request $request)
     {
         $m = self::MODEL;
+        $n = self::MODEL2;
         $this->validate($request, $m::$rules);
         $user = $request->user();
         $user_array = array('mentee_id' => $user->id, "status_id" => 2);
         $record = array_merge($request->all(), $user_array);
-        return $this->respond(Response::HTTP_CREATED, $m::create($record));
+        $mentorship_request = $m::create($record);
+        foreach ($record['skills'] as &$skill) {
+            $n::create([
+                'request_id' => $mentorship_request->id,
+                'skill_id' => $skill
+            ]);
+        }
+        return $this->respond(Response::HTTP_CREATED, $mentorship_request);
     }
 
 }
