a2a21c6463c4c70fbffb73469882e9c3411130ae
Charles Oduk <odukjr@gmail.com>
2017-10-10 20:43:42 +0300

    chore(user-ratings): display user ratings as decimals (#210)
    Currently the format of user ratings on the profile page is not
      specified.
    
      This chore gets the user rating and rounds it to one decimal place
    
      [Delivers #151675767]

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -3,7 +3,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Rating extends Model {
+class Rating extends Model
+{
     protected $table = "ratings";
     protected $primaryKey = ['session_id', 'user_id'];
     public $incrementing = false;
@@ -22,19 +23,34 @@ class Rating extends Model {
         "values" => "required|array",
         "scale" => "required|integer",
     ];
-
-    // Relationships
+    
+    /**
+     * Defines Foreign Key Relationship to the session model
+     *
+     * @return Object
+     */
     public function session()
     {
         return $this->belongsTo("App\Models\Session", "session_id", "id");
     }
-
+    
+    /**
+     * Defines Foreign Key Relationship to the user model
+     *
+     * @return Object
+     */
     public function user()
     {
         return $this->belongsTo("App\Models\User", "user_id", "user_id");
     }
     
-
+    /**
+     * Gets the ratings of the user and calculate the average
+     *
+     * @param string $userId -The logged in user's id
+     *
+     * @return int - The average rating of the user to one decimal place
+     */
     public static function getAverageRatings($userId)
     {
         $ratingValues = [];
@@ -49,7 +65,10 @@ class Rating extends Model {
             $userRatings = json_decode($rating->values);
             foreach (get_object_vars($userRatings) as $ratingName => $ratingValue) {
                 array_push($ratingValues, $ratingValue);
-                $averageRating = array_sum($ratingValues) / count($ratingValues);
+                $averageRating = number_format(
+                    (array_sum($ratingValues) / count($ratingValues)),
+                    1
+                );
             }
         }
         return $averageRating;
