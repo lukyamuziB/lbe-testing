d69a3fd45a38221d1b827ccb00c924655e4db2f2
Nnamani Kenechukwu <nnamani.kenechukwu@gmail.com>
2018-03-20 14:11:14 +0100

    chore(gcp-deployment): add auto deployment (#308)
    Currently, the GCP infrastucture does not have
    auto deployment functionality as all changes are
    deployed manually
    
    This commits enables circleci to auto deploy
    changes to GCP upon a successful test
    
    [Finishes #155934332]

---
+++ .circleci/deploy_lenken_production.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+configure_gcloud(){
+  gcloud auth activate-service-account --key-file ${HOME}/auth_lenken_app.json
+  gcloud --quiet config set project ${PROJECT_ID}
+  gcloud --quiet config set compute/zone europe-west1-b
+}
+
+deploy_change(){
+  gcloud compute project-info add-metadata --metadata production_backend_build_commit=${CIRCLE_SHA1}
+  gcloud beta compute instance-groups managed rolling-action replace production-lenken-api-group-manager --max-surge=3 --max-unavailable=0 --min-ready=200 --zone=europe-west1-b
+}
+
+main(){
+  # configure_gcloud
+  # deploy_change
+  echo "Production on GCP is not yet set up! :("
+}
+ 
+main
